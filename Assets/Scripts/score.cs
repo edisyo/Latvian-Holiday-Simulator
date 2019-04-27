a8bb9311d05524c950332081d80a9c9424654931
edisyo <45883565+edisyo@users.noreply.github.com>
2019-04-27 15:15:32 +0300

    vaga_v2
    vaga added with vr

---
+++ Assets/Scripts/score.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class score : MonoBehaviour
+{
+    public Text UIText;
+    public int currentScore = 0;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "GoodPotato") {
+            currentScore += 1;
+            UIText.text = "Score: " + currentScore.ToString();
+            Debug.Log("Meme");
+        }
+        else if(other.gameObject.tag == "BadPotato")
+        {
+            currentScore -= 1;
+            UIText.text = "Score: " + currentScore.ToString();
+        }
+    }
+
+}
